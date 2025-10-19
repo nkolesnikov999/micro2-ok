4017986db11f2ff2ef96e6cd116adc7e4ed0cba3
nkolesnikov999 <nkolesnikov999@gmail.com>
2025-10-19 21:27:52 +0300

    fix PM in payment client

--- order/internal/client/grpc/payment/v1/pay_order.go
+++ order/internal/client/grpc/payment/v1/pay_order.go
@@ -7,10 +7,24 @@ import (
 )
 
 func (c *client) PayOrder(ctx context.Context, orderUUID, userUUID, paymentMethod string) (transactionUUID string, err error) {
+	var methodEnum paymentV1.PaymentMethod
+	switch paymentMethod {
+	case "CARD":
+		methodEnum = paymentV1.PaymentMethod_PAYMENT_METHOD_CARD
+	case "SBP":
+		methodEnum = paymentV1.PaymentMethod_PAYMENT_METHOD_SBP
+	case "CREDIT_CARD":
+		methodEnum = paymentV1.PaymentMethod_PAYMENT_METHOD_CREDIT_CARD
+	case "INVESTOR_MONEY":
+		methodEnum = paymentV1.PaymentMethod_PAYMENT_METHOD_INVESTOR_MONEY
+	default:
+		methodEnum = paymentV1.PaymentMethod_PAYMENT_METHOD_UNSPECIFIED
+	}
+
 	response, err := c.paymentClient.PayOrder(ctx, &paymentV1.PayOrderRequest{
 		OrderUuid:     orderUUID,
 		UserUuid:      userUUID,
-		PaymentMethod: paymentV1.PaymentMethod(paymentV1.PaymentMethod_value[paymentMethod]),
+		PaymentMethod: methodEnum,
 	})
 	if err != nil {
 		return "", err
