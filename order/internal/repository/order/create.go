d71d1e58bce1e683a7bf9c87300e1ae00e29a3f7
nkolesnikov999 <nkolesnikov999@gmail.com>
2025-10-28 10:18:12 +0300

    change create order

--- order/internal/repository/order/create.go
+++ order/internal/repository/order/create.go
@@ -2,24 +2,14 @@ package order
 
 import (
 	"context"
+	"errors"
 
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/nkolesnikov999/micro2-OK/order/internal/model"
 	repoConverter "github.com/nkolesnikov999/micro2-OK/order/internal/repository/converter"
 )
 
 func (r *repository) CreateOrder(ctx context.Context, order model.Order) error {
-	// Сначала проверим, существует ли заказ
-	var exists bool
-	checkQuery := "SELECT EXISTS(SELECT 1 FROM orders WHERE order_uuid = $1)"
-	err := r.connDB.QueryRow(ctx, checkQuery, order.OrderUUID.String()).Scan(&exists)
-	if err != nil {
-		return err
-	}
-
-	if exists {
-		return model.ErrOrderAlreadyExists
-	}
-
 	insertQuery := `
 		INSERT INTO orders (order_uuid, user_uuid, part_uuids, total_price, 
 		                   transaction_uuid, payment_method, status)
@@ -27,7 +17,7 @@ func (r *repository) CreateOrder(ctx context.Context, order model.Order) error {
 
 	repoOrder := repoConverter.ToRepoOrder(order)
 
-	_, err = r.connDB.Exec(ctx, insertQuery,
+	_, err := r.connDB.Exec(ctx, insertQuery,
 		repoOrder.OrderUUID,
 		repoOrder.UserUUID,
 		repoOrder.PartUuids,
@@ -37,6 +27,11 @@ func (r *repository) CreateOrder(ctx context.Context, order model.Order) error {
 		repoOrder.Status,
 	)
 	if err != nil {
+		// Проверяем, является ли ошибка нарушением ограничения уникальности
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return model.ErrOrderAlreadyExists
+		}
 		return err
 	}
 
